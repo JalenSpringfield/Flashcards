84261fdd044cdb7d66521149dc5243cc2dc22e8e
Jalen Springfield <springfield.jalen@gmail.com>
2018-11-03 19:12:56 -0400

    Added functionality to edit the question and answer of a flashcard

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        creationController.flashcardsController = self
+        
+    }
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         if(frontLabel.isHidden)
         {
@@ -23,6 +31,10 @@ class ViewController: UIViewController {
         frontLabel.isHidden = true
         }
         print(frontLabel.isHidden)
+    }
+    func updateFlashcard(question: String, answer: String) {
+        frontLabel.text = question
+        backLabel.text = answer
     }
     override func viewDidLoad() {
         super.viewDidLoad()
